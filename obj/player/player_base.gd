afff8a5d01d14afc7b3addf955afc0cde5bee88d
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2024-08-16 13:19:10 -0700

    diaogic, setup

---
+++ obj/player/player_base.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+const ACCELERATION: float = 80
+
+var latest_kid: int = 0
+var kid_count: int = 1
+
+var talking: bool = false
---
+++ systems/quests/quest_resource.gd
@@ -0,0 +1,5 @@
+class_name QuestResource
+extends Resource
+
+@export var display_require: String
+@export var count: int = 0
