259abd94d22f37be022c64920d9867315b956c34
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2024-08-16 09:49:37 -0700

    boilerplate

---
+++ scripts/components/state.gd
@@ -0,0 +1,23 @@
+class_name State
+extends Node
+
+var machine : StateMachine
+
+func _enter(msg := {}) -> void:
+	pass
+
+
+func _handle_input(_event: InputEvent) -> void:
+	pass
+
+
+func _update(_delta: float) -> void:
+	pass
+
+
+func _physics_update(_delta: float) -> void:
+	pass
+
+
+func _exit() -> void:
+	pass
